80438fdf24a075ac2edccc2da12330107800dc33
priyanka593155 <pri497243@gmail.com>
2024-03-03 22:52:57 +0530

    we changed the log

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ dotenv.config()
 connectDB()
 .then(()=>{
   app.listen(process.env.PORT || 8000, ()=>{
-    console.log(`server is running at port : ${process.env.PORT}`);
+    // console.log(`server is running at port : ${process.env.PORT}`);
+    console.log("server is up and running")
   })
 })
 .catch((err)=> {
   console.log("momodb connection failed !!!", err);
 })
-
 
 
 
